a810ba0caf472323d5a8cc195cdfbde6fde5bb68
Jared Williams <jaredwilliamsmail@gmail.com>
2015-05-13 22:13:19 -0500

    flash msg contact mailer pt 1

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,5 +4,19 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save?
+      flash[:success] = 'Success! Your message has been sent.'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Error, please try again.'
+      redirect_to new_contact_path
+    end
   end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end

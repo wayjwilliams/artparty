195b44acd66902f7c13bd78f5e33baf3d9068b57
Jared Williams <jaredwilliamsmail@gmail.com>
2015-05-16 15:44:16 -0500

    adj devise controllers and model for stripe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150515202936) do
+ActiveRecord::Schema.define(version: 20150516203956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150515202936) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "plan_id"
+    t.string   "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

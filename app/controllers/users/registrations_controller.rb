3915954dffe36b601fe5f81d8590da0d57e9d66a
Jared Williams <jaredwilliamsmail@gmail.com>
2015-05-16 20:11:15 -0500

    add bs alert and membership select only

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :select_plan, only: :new
 
   def create
     super do |resource|
@@ -12,3 +13,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+
+  private
+    def select_plan
+      unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select Basic or Premium, yo."
+        redirect_to root_url
+      end
+    end
+end
